47c2c3a9c48213b731ecd4fa098fe8340a5d12e5
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-03 19:11:24 +0100

    drop patches

--- gazebo-pkgs/jetty/gz-common7.nix
+++ gazebo-pkgs/jetty/gz-common7.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
 
   assimp,
   cmake,
@@ -31,17 +30,6 @@ stdenv.mkDerivation {
     tag = "gz-common7_7.1.0";
     hash = "sha256-CtQL2/ndK+vcNoIzBiGLI+S+GYDH3ID9nC8GxmzcpJ4=";
   };
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/gazebosim/gz-common/commit/58c6eaa7bd9c048264f3e0b33a36a744cca8d18c.patch?full_index=1";
-      hash = "sha256-1MDiufVeNj54sWs0H200INVxS8mwzwR1UVMd523Byp0=";
-    })
-    (fetchpatch {
-      url = "https://github.com/wentasah/gz-common/commit/8d18342302b7586b5b34c3cd12f2ef26e148b6ab.patch?full_index=1";
-      hash = "sha256-Jtx1IShjLjaKsQ0x9RmTwPpbd3x07dOrt7bEXMTGnCw=";
-    })
-  ];
 
   nativeBuildInputs = [
     cmake
