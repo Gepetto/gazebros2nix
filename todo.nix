0d8e7981165dc75724c3988bebc43d01a134c928
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-06-22 14:25:58 +0200

    nix fmt

--- todo.nix
+++ todo.nix
@@ -555,10 +555,10 @@ final: prev: {
           net-ft-driver = jazzy-prev.net-ft-driver.overrideAttrs {
             src = jazzy-final.net-ft-description.src;
           };
-          odri-dual-motor-testbed-description = jazzy-prev.odri-dual-motor-testbed-description.overrideAttrs {
+          odri-dual-motor-testbed-bringup = jazzy-prev.odri-dual-motor-testbed-bringup.overrideAttrs {
             doCheck = false; # TODO: cppcheck + cpplint + uncrustify
           };
-          odri-dual-motor-testbed-bringup = jazzy-prev.odri-dual-motor-testbed-bringup.overrideAttrs {
+          odri-dual-motor-testbed-description = jazzy-prev.odri-dual-motor-testbed-description.overrideAttrs {
             doCheck = false; # TODO: cppcheck + cpplint + uncrustify
           };
           pal-gazebo-plugins = null;
